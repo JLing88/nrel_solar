2b8e5cdc5a2c1359794ba1279c5aac1dbf6d08cc
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-10 11:43:05 -0700

    Continued progress on search bar, home page, & property details page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'locations#index'
-  resources :locations, only: [:index]
-#   get '/locations', to: "locations#index"
+  root to: 'home#index'
+  get '/property', to: 'properties#show', as: 'property'
 end
