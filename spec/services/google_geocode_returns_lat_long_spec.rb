900a4e972b3e0fb9d513deece2b67af60b0be9e2
mmbensalah <mmbensalah@gmail.com>
2018-11-10 09:51:14 -0700

    creates service test

--- spec/services/google_geocode_returns_lat_long_spec.rb
+++ spec/services/google_geocode_returns_lat_long_spec.rb
@@ -6,4 +6,14 @@ describe 'Google Geocode API' do
 
     expect(service).to be_a(GoogleGeocodeService)
   end
+
+  it 'gets the data' do
+  # VCR.use_cassette("GoogleGeocodeService") do
+      address = "1331 17th Street, Denver, CO 80202"
+      facade = WeatherFacade.new(address)
+      lat_long = facade.coordinates
+      expect(lat_long[:lat]).to eq(39.7507834)
+      expect(lat_long[:lng]).to eq(-104.9964355)
+  # end
+  end
 end
